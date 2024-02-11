e983b75bcf55140cb3461a0d440969b49e0732d3
shahhussain-t <shahhussaint6@gmail.com>
2024-02-11 07:13:07 +0500

    little bit changes

--- server/src/index.js
+++ server/src/index.js
@@ -1,13 +1,13 @@
 require('dotenv').config();
 const cors = require('cors');
-const express=require('express')
+const express = require('express')
 const bodyParser = require('body-parser');
 const userRoutes = require('./routes/userRouter');
 const bookRoutes = require('./routes/bookRouter');
 const cookieParser = require('cookie-parser');
 const connectDB = require('./db/connection');
 
-const app=express()
+const app = express()
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -18,24 +18,16 @@ app.use(cors({ origin: 'http://localhost:5173' }));
 connectDB();
 
 
-
-
-
-
-
-
-const PORT = process.env.PORT || 3000; 
+const PORT = process.env.PORT || 3000;
 
 
 app.use(express.json());
 app.use(cookieParser());
 
 
-
-
+  
 app.use('/api/users', userRoutes);
 app.use('/api/books', bookRoutes);
-
 
 
 
