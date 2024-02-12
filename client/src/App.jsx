b8bbc202fe33c7ba7a64a84dd13de5024371610e
shahhussain-t <shahhussaint6@gmail.com>
2024-02-12 22:59:54 +0500

    tailwind css setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,15 @@
-import React, { useEffect,useState } from 'react';
+
 
 
 function App() {
-const [data,setData]=useState("")
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const backendUrl = `http://localhost:3000`;
-        const response = await fetch(`${backendUrl}/api`);
-        const data = await response.json();
-        setData(data)
-        console.log('Data from backend:', data);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
 
-    fetchData();
-  }, []);
+
 
   return (
     <>
-     <h1>{data.data}</h1>
+     <h1>Hello world</h1>
+ 
     </>
   )
 }
